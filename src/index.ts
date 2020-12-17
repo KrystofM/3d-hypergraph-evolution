9b5f760851b670a60703cfa68e822783b4b2fd10
Krystof Mitka <krystof.mitka@seznam.cz>
2020-12-17 14:53:39 +0100

    Project: Add Shopify ESLint

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
-import * as _ from 'lodash';
-import * as THREE from 'three';
+import * as _ from "lodash";
+import * as THREE from "three";
 
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement("div");
 
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
 
-    return element;
+  return element;
 }
 
 document.body.appendChild(component());
 
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
